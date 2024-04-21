5c5fe842ec64a845f8f58eff9353212cc4a70563
Anthony <syspuke@pm.me>
2024-04-21 05:21:14 -0500

    mistralapi

--- airoboros/self_instruct.py
+++ airoboros/self_instruct.py
@@ -504,16 +504,14 @@ class SelfInstructor:
         filter_response = kwargs.pop("filter_response", True)
         model = kwargs.get("model", self.model)
 
-        client = MistralAsyncClient(api_key=self.mistral_api_token, timeout=600.0)
+        client = MistralClient(api_key=self.mistral_api_token, timeout=600.0)
 
-        chat_response = await client.chat(
+        chat_response = client.chat(
             model=model,
             messages=[ChatMessage(role="user", content=instruction)]
         )
         
         text = chat_response.choices[0].message.content
-        
-        await client.close()
         
         if filter_response:
             for banned in self.response_filters:
